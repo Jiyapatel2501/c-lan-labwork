f27c1aa946afa727543b0f7e76268198db8d8544
Jiyapatel2501 <pdrashti189@gmail.com>
2026-04-29 12:00:30 +0530

    Nested if

---
+++ lecture-6-flowchart-nestedif/max-of-6.c
@@ -0,0 +1,172 @@
+#include<stdio.h>
+int main(){
+	int a,b,c,d,e,f;
+	printf("Enter the first value :");
+	scanf("%d",&a);
+	printf("Enter the second value :");
+	scanf("%d",&1b);
+	printf("Enter the third value :");
+	scanf("%d",&c);
+	printf("Enter the forth value :");
+	scanf("%d",&d);
+	printf("Enter the fifth value :");
+	scanf("%d",&e);
+	printf("Enter the sixth value :");
+	scanf("%d",&f);
+	if(a>=b){
+		//a is max
+		if(a>=c){
+			//a is max
+			if(a>=d){
+				//a is max
+				if(a>=e){
+					//a is max
+					if(a>=f){
+						printf("%d is max",a);
+					}else{
+						printf("%d is max",f);
+					}
+				}else{
+					//e is max
+					if(e>=f){
+						printf("%d is max",e);
+					}else{
+						printf("%d is max",f);
+					}
+				}
+			}else{
+				//d is max
+				if(d>=e){
+					//d is max
+					if(d>=f){
+						printf("%d is max",d);
+					}else{
+						printf("%d is max",f);
+					}
+				}else{
+					//e is max
+					if(e>=f){
+						printf("%d is max",e);
+					}else{
+						printf("%d is max",f);
+					}
+				}
+			}
+		}else{
+			//c is max
+			if(c>=d){
+				//c is max
+				if(c>=e){
+					//c is max
+					if(c>=f){
+						printf("%d is max",c);
+					}else{
+						printf("%d is max",f);
+					}
+				}else{
+					//e is max
+					if(e>=f){
+						printf("%d is max",e);
+					}else{
+						printf("%d is max",f);
+					}
+				}
+			}else{
+				//d is max
+				if(d>=e){
+					//d is max
+					if(d>=f){
+						printf("%d is max",d);
+					}else{
+						printf("%d is max",f);
+					}
+				}else{
+					//e is max
+					if(e>=f){
+						printf("%d is max",e);
+					}else{
+						printf("%d is max",f);
+					}
+				}
+			}
+		}
+	}else{
+		//b is max
+		if(b>=c){
+			//b is max
+			if(b>=d){
+				//b is max
+				if(b>=e){
+					//b is max
+					if(b>=f){
+						printf("%d is max",b);	
+					}else{
+						printf("%d is max",f);
+					}
+				}else{
+					//e is max
+					if(e>=f){
+						printf("%d is max",e);
+					}else{
+						printf("%d is max",f);	
+					}
+				}
+			}else{
+				//d is max
+				if(d>=e){
+					//d is mmax
+					if(d>=f){
+						printf("%d is max",d);
+					}else{
+						printf("%d is max",f);
+					}
+				}else{
+					//e is max
+					if(e>=f){
+						printf("%d is max",e);
+					}else{
+						printf("%d is max",f);
+					}
+				}
+			}
+		}else{
+			//c is max
+			if(c>=d){
+				//c is max
+				if(c>=e){
+					//c is max
+					if(c>=f){
+						printf("%d is max",c);
+					}else{
+						printf("%d is max",f);
+					}
+				}else{
+					//e is max
+					if(e>=f){
+						printf("%d is max",e);
+					}else{
+						printf("%d is max",f);
+					}
+				}
+			}else{
+				//d is max
+				if(d>=e){
+					//d is max
+					if(d>=f){
+						printf("%d is max",d);
+					}else{
+						printf("%d is max",f);
+					}
+				}else{
+					//e is max
+					if(e>=f){
+						printf("%d is max",e);	
+					}else{
+						printf("%d is max",f);
+					}
+				}
+			}
+		}
+	}
+	
+}
